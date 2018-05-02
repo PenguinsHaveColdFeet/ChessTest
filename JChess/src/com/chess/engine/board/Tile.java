612baf814e51ecd692555c758af46f61e3d36220
Penguins <fdsfsd@dsafds>
2018-05-02 21:16:56 +0200

    see above

--- JChess/src/com/chess/engine/board/Tile.java
+++ JChess/src/com/chess/engine/board/Tile.java
@@ -1,18 +1,39 @@
+package com.chess.engine.board;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
 
 public abstract class Tile {
 
-	int tileCoordinate;
+	protected final int tileCoordinate;
+
+	private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
 	Tile(int tileCoordinate) {
 		this.tileCoordinate = tileCoordinate;
+	}
+
+	private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+		final Map<Integer, EmptyTile> emptyTilesMap = new HashMap<>();
+		for (int i = 0; i < 64; i++) {
+			emptyTilesMap.put(i, new EmptyTile(i));
+		}
+		return ImmutableMap.copyOf(emptyTilesMap);
+	}
+
+	public static Tile createTile(final int tileCoordinate, Piece piece) {
+		return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
 	}
 
 	public abstract boolean isTileOccupied();
 
 	public abstract Piece getPiece();
 
-	public static final class EmptyTile extends Tile {
-		EmptyTile(int coordinate) {
+	private static final class EmptyTile extends Tile {
+		EmptyTile(final int coordinate) {
 			super(coordinate);
 		}
 
@@ -28,8 +49,8 @@ public abstract class Tile {
 
 	}
 
-	public static final class OccupiedTile extends Tile {
-		Piece pieceOnTile;
+	private static final class OccupiedTile extends Tile {
+		private final Piece pieceOnTile;
 
 		OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
 			super(tileCoordinate);
